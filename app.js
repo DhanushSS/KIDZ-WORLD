f44c5af8039319a40e571af46d9d5f747a6c4cb4
DhanushSS <67519006+DhanushSS@users.noreply.github.com>
2020-11-29 22:55:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var cartValue = document.getElementById("cart-value");
+var cartButtons=document.getElementById("cart");
+
+var addBook1 = document.getElementById("book1");
+var addBook2 = document.getElementById("book2");
+var addBook3 = document.getElementById("book3");
+var addBook4 = document.getElementById("book4");
+var addBook5 = document.getElementById("book5");
+var addBook6 = document.getElementById("book6");
+
+var addGame1 = document.getElementById("games1");
+var addGame2 = document.getElementById("games2");
+var addGame3 = document.getElementById("games3");
+var addGame4 = document.getElementById("games4");
+
+var addCraft1 = document.getElementById("craft1");
+var addCraft2 = document.getElementById("craft2");
+var addCraft3 = document.getElementById("craft3");
+var addCraft4 = document.getElementById("craft4");
+
+console.log(book1);
